cefeba1199749a467541ccf7766bfb53d8b0fe9a
ratkaya <ratkay.andras@gmail.com>
2022-09-19 16:11:22 +0200

    Implement basic CRUD service for Person Entity
    Wit appropriate HTTP requests the contents of the connected database can
    be modified. A person row on public.person table can be read, added,
    updated and deleted.

--- src/main/kotlin/hu/babobeo/app/database/dto/PersonDto.kt
+++ src/main/kotlin/hu/babobeo/app/database/dto/PersonDto.kt
@@ -2,4 +2,5 @@ package hu.babobeo.app.database.dto
 
 import java.time.Instant
 
-data class PersonDto(val firstName: String, val lastName: String, val dateOfBirth: Instant)
+data class ExistingPersonDto(val id: Long, val firstName: String, val lastName: String, val dateOfBirth: Instant)
+data class NewPersonDto(val firstName: String, val lastName: String, val dateOfBirth: Instant)
--- src/main/kotlin/hu/babobeo/app/database/mappers/PersonMapper.kt
+++ src/main/kotlin/hu/babobeo/app/database/mappers/PersonMapper.kt
@@ -1,11 +1,18 @@
 package hu.babobeo.app.database.mappers
 
-import hu.babobeo.app.database.dto.PersonDto
+import hu.babobeo.app.database.dto.ExistingPersonDto
+import hu.babobeo.app.database.dto.NewPersonDto
 import hu.babobeo.app.database.entities.Person
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 
 @Mapper
 interface PersonMapper {
 
-    fun entityToDatamodel(person: Person): PersonDto
+    fun entityToDatamodel(person: Person): ExistingPersonDto
+
+    @Mapping(target = "id", ignore = true)
+    fun newDatamodelToEntity(existingPersonDto: NewPersonDto): Person
+
+    fun datamodelToEntity(existingPersonDto: ExistingPersonDto): Person
 }
